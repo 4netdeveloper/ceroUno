21b77e3d78745250f613a68756944e9be4906e7b
programa00leo30 <programa00leo30@gmail.com>
2019-05-26 13:20:56 -0300

    creacion de administrador

--- app/src/main/java/com/example/cerouno/ambiente.java
+++ app/src/main/java/com/example/cerouno/ambiente.java
@@ -1,51 +1,81 @@
-package com.example.cerouno;
+package com.example.cerouno.manejadores;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class ambiente extends AppCompatActivity {
+import com.example.cerouno.R;
+import com.example.cerouno.manejadores.MenuSlideActivity;
 
-    private final int[] BOTONESMENU = {R.id.amb_tv, R.id.amb_cocina, R.id.amb_comedor, R.id.amb_bano, R.id.amb_dormitorio,  //array de botones
-            R.id.amb_dormitorio2, R.id.amb_patio, R.id.amb_extra};
+public class ambiente extends AppCompatActivity implements View.OnClickListener{
 
-     Fragment [] misFragmentos;
+    static int[] BOTONESMENU = {R.id.amb_tv, R.id.amb_cocina, R.id.amb_comedor, R.id.amb_bano, R.id.amb_dormitorio, R.id.amb_dormitorio2,  //array de botones
+            R.id.amb_patio};
+
+    int i, j;
+    int opcion = i;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambiente);
 
-        misFragmentos [0] = new Living();
-        misFragmentos [1] = new Cocina();
-        misFragmentos [2] = new Comedor();
-        misFragmentos [3] = new Bano();
-        misFragmentos [4] = new Dormitorio();
-        misFragmentos [5] = new Patio();
+        ImageButton livingB = findViewById(BOTONESMENU[0]);
+        livingB.setOnClickListener(this);
 
-        //en esta variable se almacena el valor de cada boton
-        ImageButton botonMenu;
+        ImageButton cocinaB = findViewById(BOTONESMENU[1]);
+        cocinaB.setOnClickListener(this);
 
-        for (int i=0; i<BOTONESMENU.length; i++){
-            for (int j = 0; j<misFragmentos.length; j++){
+        ImageButton comedorB = findViewById(BOTONESMENU[2]);
+        comedorB.setOnClickListener(this);
 
-                botonMenu = findViewById(BOTONESMENU[i]);
+        ImageButton banoB = findViewById(BOTONESMENU[3]);
+        banoB.setOnClickListener(this);
 
-                botonMenu.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+        ImageButton dormitorioB = findViewById(BOTONESMENU[4]);
+        dormitorioB.setOnClickListener(this);
 
-                        Intent estaActividad = getApplicationContext(this, misFragmentos[j]);
-                        startActivity(estaActividad);
-                    }
-                });
+        ImageButton dormitorio2B = findViewById(BOTONESMENU[5]);
+        dormitorio2B.setOnClickListener(this);
 
-            }
-        }
-
+        ImageButton patioB = findViewById(BOTONESMENU[6]);
+        patioB.setOnClickListener(this);
     }
 
+    private void cargarFragmento (Fragment fragmento){
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.contenedor, fragmento).commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.amb_tv: Intent intent = new Intent(getApplicationContext(), MenuSlideActivity.class);
+                startActivity(intent); MenuSlideActivity.opcion= 1 ;break;
+
+            case R.id.amb_cocina: intent = new Intent(getApplicationContext(),MenuSlideActivity.class);
+                startActivity(intent); MenuSlideActivity.opcion= 2 ;break;
+
+            case R.id.amb_comedor: intent = new Intent(getApplicationContext(),MenuSlideActivity.class);
+                startActivity(intent); MenuSlideActivity.opcion= 3 ;break;
+
+            case R.id.amb_bano: intent = new Intent(getApplicationContext(),MenuSlideActivity.class);
+                startActivity(intent); MenuSlideActivity.opcion= 4 ;break;
+
+            case R.id.amb_dormitorio: intent = new Intent(getApplicationContext(),MenuSlideActivity.class);
+                startActivity(intent); MenuSlideActivity.opcion= 5 ;break;
+
+            case R.id.amb_dormitorio2: intent = new Intent(getApplicationContext(),MenuSlideActivity.class);
+                startActivity(intent); MenuSlideActivity.opcion= 6 ;break;
+
+            case R.id.amb_patio: intent = new Intent(getApplicationContext(),MenuSlideActivity.class);
+                startActivity(intent); MenuSlideActivity.opcion= 7 ;break;
+        }
+    }
 }
